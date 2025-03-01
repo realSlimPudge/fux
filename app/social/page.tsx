84aa1c4c09be7c976924cac73cd2d2e2a746f66f
realSlimPudge <komissarov010806@gmail.com>
2025-03-01 20:53:05 +0300

    rofl

--- app/social/page.tsx
+++ app/social/page.tsx
@@ -37,6 +37,9 @@ export default function Social() {
                         ))}
                     </ul>
                 </div>
+                <div className="absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%]">
+                    Илья дубинский говно
+                </div>
                 <div>
                     <button
                         onClick={() => {
