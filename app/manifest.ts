7ed45b37281165321e2454e0dd5a3987ab291437
realSlimPudge <komissarov010806@gmail.com>
2025-03-06 23:13:26 +0300

    Добавлены иконки

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,7 +8,21 @@ export default function manifest(): MetadataRoute.Manifest {
             "Создавайте SMART-цели, делитесь с другими пользователями, достигайте результата",
         start_url: "/",
         display: "standalone",
+        icons: [
+            {
+                src: "/web-app-manifest-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+                purpose: "maskable",
+            },
+            {
+                src: "/web-app-manifest-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable",
+            },
+        ],
         background_color: "#f3f4f6",
-        theme_color: "#000000",
+        theme_color: "#f3f4f6",
     };
 }
